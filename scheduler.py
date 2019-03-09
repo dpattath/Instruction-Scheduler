38e4f45cd751981e0cbad5491d3419d0a0a67bb4
Daniel Pattathil <dp865@ilab3.cs.rutgers.edu>
2019-03-08 20:06:24 -0500

    fixed for storeAO and loadAO

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,9 @@ import sys
 #easier ways to check which type of instruction we're looking at
 math = set(['add', 'sub', 'mult', 'div'])
 address = set(['loadAI', 'storeAI', 'outputAI'])
+loads = set(['load', 'loadAI', 'loadAO'])
+stores = set(['store', 'storeAI', 'storeAO'])
+
 
 #dict to map instruction types to their cycle counts
 cycles = {
@@ -65,22 +68,27 @@ def leftparams(instruction):
 	op = instruction.opcode
 	if op == 'loadI':
 		return None
-	if (op in math):
+	if (op in math) or (op == 'loadAO'):
 		return [instruction.field1, instruction.field2]
 	if (op == 'outputAI') or (op == 'loadAI'):
 		return (instruction.field1, instruction.field2)
-	if op == 'storeAI':
+	if (op == 'storeAI') or (op == 'storeAO') or (op == 'load') or (op == 'store'):
 		return instruction.field1
+		
 
 #gives the parameters right of the '=>'
 def rightparams(instruction):
 	op = instruction.opcode
-	if op == 'loadI':
+	if (op == 'loadI') or (op == 'load') or (op == 'store'):
 		return instruction.field2
-	if (op in math) or (op == 'outputAI') or (op == 'loadAI'):
+	if (op in math) or (op == 'loadAI') or (op == 'loadAO'):
 		return instruction.field3
 	if op == 'storeAI':
 		return (instruction.field2, instruction.field3)
+	if op == 'storeAO':
+		return [instruction.field2, instruction.field3]
+	if op == 'outputAI':
+		return None
 
 #for inst in instructions:
 #	print(inst.opcode + "\t" +  inst.field1 + "\t" +  inst.field2 + "\t" +  inst.field3 + "\tcycles: " + str(inst.cycles))
@@ -98,6 +106,14 @@ for i in range(len(instructions)):
 			if(op_j in address):
 				parents[j].add(i)
 				children[i].add(j)
+			if(op_j == 'storeAO'):
+				if 'r0' in rightparams(inst_j):
+					parents[j].add(i)
+					children[i].add(j)
+			if(op_j == 'loadAO'):
+				if 'r0' in leftparams(inst_j):
+					parents[j].add(i)
+					children[i].add(j)
 		leaves.add(i)
 		continue
 	
@@ -111,39 +127,116 @@ for i in range(len(instructions)):
 				children[i].add(j)
 				break
 
-	#iterate through the rest of the inst's and check for dependence
-	for j in range(i+1, len(instructions)):
-		inst_j = instructions[j]
-		op_j = inst_j.opcode
-		if op_j == 'loadI':
-			continue
-		if op_j in math:
-			if rightparams(inst_i) in leftparams(inst_j):
+	#all loads must happen before stores
+	if(op_i in loads):
+		for j in range(i+1, len(instructions)):
+			inst_j = instructions[j]
+			op_j = inst_j.opcode
+			if(op_i == 'loadAI') and (op_j == 'storeAI'):
+				continue 
+			if(op_j in stores):
 				parents[j].add(i)
 				children[i].add(j)
-		if rightparams(inst_i) == leftparams(inst_j):
-			parents[j].add(i)
-			children[i].add(j)
-		#break when you find that this register has been reset
-		if rightparams(inst_i) == rightparams(inst_j):
-			break
 
-	#iterate through previous inst's and check for anti dependence
-	for j in reversed(range(0, i)):
-		inst_j = instructions[j]
-		op_j = inst_j.opcode
-		if op_j == 'loadI':
-			continue
-		if op_j in math:
-			if rightparams(inst_i) in leftparams(inst_j):
+	#all stores must happen before output
+	if(op_i in stores):
+		for j in range(i+1, len(instructions)):
+			inst_j = instructions[j]
+			op_j = inst_j.opcode
+			if(op_i == 'storeAI') and (op_j == 'loadAI'):
+				continue 
+			if(op_j in loads):
+				parents[j].add(i)
+				children[i].add(j)
+			if(op_j == 'outputAI'):
+				parents[j].add(i)
+				children[i].add(j)
+			
+	#special case for storeAO for setting TRUE dependences
+	if(op_i == 'storeAO'):
+		for parameter in rightparams(inst_i):
+			if(parameter == 'r0'):
+				continue
+			for j in range(i+1, len(instructions)):
+				inst_j = instructions[j]
+				op_j = inst_j.opcode
+				if (op_j == 'loadI') or (op_j == 'outputAI'):
+					continue
+				if (op_j in math) or (op_j == 'loadAO'):
+					if parameter in leftparams(inst_j):
+						parents[j].add(i)
+						children[i].add(j)
+				if(op_j == 'storeAO'):
+					if parameter in rightparams(inst_j):
+						parents[j].add(i)
+						children[i].add(j)
+				if parameter == leftparams(inst_j):
+					parents[j].add(i)
+					children[i].add(j)
+				#break when you find that this register has been reset
+				if parameter == rightparams(inst_j):
+					break
+
+	else:
+	#iterate through the rest of the inst's and check for dependence
+		for j in range(i+1, len(instructions)):
+			inst_j = instructions[j]
+			op_j = inst_j.opcode
+			if (op_j == 'loadI') or (op_j == 'outputAI'):
+				continue
+			if (op_j in math) or (op_j == 'loadAO'):
+				if rightparams(inst_i) in leftparams(inst_j):
+					parents[j].add(i)
+					children[i].add(j)
+			#forgot to accommodate for storeAO using only SET, no DEF
+			if(op_j == 'storeAO'):
+					if rightparams(inst_i) in rightparams(inst_j):
+						parents[j].add(i)
+						children[i].add(j)
+			if rightparams(inst_i) == leftparams(inst_j):
+				parents[j].add(i)
+				children[i].add(j)
+			#break when you find that this register has been reset
+			if rightparams(inst_i) == rightparams(inst_j):
+				break
+
+	#special case for AO
+	if(op_i == 'storeAO'):
+		for parameter in rightparams(inst_i):
+			if(parameter == 'r0'):
+				continue
+			for j in reversed(range(0, i)):
+				inst_j = instructions[j]
+				op_j = inst_j.opcode
+				if (op_j == 'loadI') or (op_j == 'outputAI'):
+					continue
+				if (op_j in math) or (op_j == 'loadAO'):
+					if parameter in leftparams(inst_j):
+						parents[i].add(j)
+						children[j].add(i)
+				if parameter == leftparams(inst_j):
+					parents[i].add(j)
+					children[j].add(i)
+				#break when you find that this register has been reset
+				if parameter == rightparams(inst_j):
+					break
+	else:
+		#iterate through previous inst's and check for anti dependence
+		for j in reversed(range(0, i)):
+			inst_j = instructions[j]
+			op_j = inst_j.opcode
+			if (op_j == 'loadI') or (op_j == 'outputAI'):
+				continue
+			if (op_j in math) or (op_j == 'loadAO'):
+				if rightparams(inst_i) in leftparams(inst_j):
+					parents[i].add(j)
+					children[j].add(i)
+			if rightparams(inst_i) == leftparams(inst_j):
 				parents[i].add(j)
 				children[j].add(i)
-		if rightparams(inst_i) == leftparams(inst_j):
-			parents[i].add(j)
-			children[j].add(i)
-		#break when you find that this register has been reset
-		if rightparams(inst_i) == rightparams(inst_j):
-			break
+			#break when you find that this register has been reset
+			if rightparams(inst_i) == rightparams(inst_j):
+				break
 
 	#if this item has no parents, add it to the leaves set
 	if parents[i] == set():
